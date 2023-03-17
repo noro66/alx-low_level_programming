126b168fec50c1426f6a033990a897e28d8c4c9d
Nouaamane <noro6@gmail.com>
2023-03-16 18:40:32 -0700

    laldf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,14 @@
-#!bin/bash
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{ printf("size of a char: %d byte(s)\n", sizeof(char));
+{printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
- printf("size of a long int: %d byte(s)\n", sizeof(long int));
- printf("size of a long: %d byte(s)\n", sizeof(long));
- printf("size of a char: %d byte(s)\n", sizeof(float));
-    return (0);
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long: %d byte(s)\n", sizeof(long));
+printf("size of a char: %d byte(s)\n", sizeof(float));
+return (0);
 }
