7d6c18cd47447f3de1ec9041dd60f8a8e076470b
Nouaamane <noro6@gmail.com>
2023-03-18 17:29:54 -0700

    ffj

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd>
+/**
+ * main - Entry point.
+ *
+ * Description: print a c code using quote function.
+ * Return: 1 (not succes)
+ */
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1.quo.59);
+return (1);
+}
