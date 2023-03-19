48bc165df834ec4ccf409f552d72a73e0398eab2
Nouaamane <noro6@gmail.com>
2023-03-18 17:39:35 -0700

    hgls

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd>
+#include <unistd.h>
 /**
  * main - Entry point.
  *
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.quo.59);
+	write(1, quo, 59);
 return (1);
 }
