34ea55d8956bcb69628838a78042edb1fa1a8e7a
Nouaamane <noro6@gmail.com>
2023-03-20 19:02:08 -0700

    fjsl

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main -Entry point
  *
  * Return: Always 0 (success)
  *
  */
+
 int main(void)
-	{
-		char c;
+{
+	char c;
 
-		c = 'a';
-
-		while (c <= 'z')
+	c = 'a';
+	while
+		(c <= 'z')
 		{
 		putchar(c);
 		c++;
 		}
-		putchar("\n");
-		return (0);
-
+	putchar("\n");
+	return (0);
+}
