5774567d764aa6688e7ca1ca0ed229789e54906b
Nouaamane <noro6@gmail.com>
2023-03-21 18:14:54 -0700

    fjsl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - print _putchar
  *
  * Description: prints _putchar using prototype
  *
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * _putchar - writers the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The characters to print
  *
  * Return: On Success 1.
- * On error, -1 is returned. and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
