8b7c1341e1fcfae56f7fad6743c3e3b6f9e70fe8
Nouaamane <noro6@gmail.com>
2023-03-21 17:14:19 -0700

    fjls

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,11 +2,11 @@
 #include <main.h>
 
 /**
- * _putchar - writers the character c to stdout 
- * @c: The characters to print 
+ * _putchar - writers the character c to stdout
+ * @c: The characters to print
  *
  * Return: On Success 1.
- * 	   On error, -1 is returned. and errno is set appropriately.
+ * On error, -1 is returned. and errno is set appropriately.
  */
 int _putchar(char c)
 {
