8aad4a76f79c1cc73ec90c7b37d52240dfc4897d
Nouaamane <noro6@gmail.com>
2023-04-20 17:11:11 -0700

    varia

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,14 +16,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	
 	va_start(tst, n);
-	
 	for (i = 0; i < n; i++)
 		sum += va_arg(tst, int);
-	
 	va_end(tst);
-	
 	return (sum);
 }
 
