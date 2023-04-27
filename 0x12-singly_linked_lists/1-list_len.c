0464157ea53580e084a7272f9b48cd7e7648e7f0
Nouamane <norou6@gmail.com>
2023-04-27 10:13:11 -0700

    done

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - count lists .
+ *
+ * @h: pinter to the first node .
+ *
+ * Return:  number of elements.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t x;
+
+	while (h)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
